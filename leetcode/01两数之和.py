5b20486d6ea8c88657f23925b74f327603118bfe
fairyPeng <935375476@qq.com>
2019-10-30 15:08:20 +0800

    修复错误以及添加注释

--- leetcode/01两数之和.py
+++ leetcode/01两数之和.py
@@ -16,12 +16,14 @@
 
 class Solution(object):
     def twoSum1(self, nums, target):
+        """两层循环,简单粗暴"""
         for i in range(len(nums)):
-            for n in range(i, len(nums)):
+            for n in range(i+1, len(nums)):
                 if nums[i] + nums[n] == target:
                     return i, n
 
     def twoSum2(self, nums, target):
+        """一次循环获取所有的索引，将索引与值拼成一个字典，求出目标值与当前值的差值，遍历num，判断差值在不在字典里面"""
         index = [i for i in range(len(nums))]
         data_dict = dict(zip(nums, index))
         for nu in range(len(nums)):
@@ -30,6 +32,7 @@ class Solution(object):
                 return nu, data_dict[difference]
 
     def twoSum3(self, nums, target):
+        """创建一个字典，遍历数组，求出目标值与当前值的差值，如果差值在字典里面，返回"""
         data_dict = {}
         for i in range(len(nums)):
             difference = target - nums[i]
