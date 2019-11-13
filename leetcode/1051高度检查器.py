696559ef5c65310418513894558156b5daf208d8
fairyPeng <935375476@qq.com>
2019-11-13 18:13:28 +0800

    leetcode

---
+++ leetcode/1051高度检查器.py
@@ -0,0 +1,43 @@
+#coding:utf-8
+"""
+学校在拍年度纪念照时，一般要求学生按照 非递减 的高度顺序排列。
+
+请你返回至少有多少个学生没有站在正确位置数量。该人数指的是：能让所有学生以 非递减 高度排列的必要移动人数。
+
+ 
+
+示例：
+
+输入：[1,1,4,2,1,3]
+输出：3
+解释：
+高度为 4、3 和最后一个 1 的学生，没有站在正确的位置。
+ 
+
+提示：
+
+1 <= heights.length <= 100
+1 <= heights[i] <= 100
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/height-checker
+
+"""
+
+
+class Solution(object):
+    def heightChecker(self, heights):
+        """
+        :type heights: List[int]
+        :rtype: int
+        """
+        asc_height = sorted(heights)
+        count = 0
+        for h in range(len(heights)):
+            if heights[h] != asc_height[h]:
+                count += 1
+        return count
+
+heights = [1,1,4,2,1,3]
+s = Solution()
+print(s.heightChecker(heights))
